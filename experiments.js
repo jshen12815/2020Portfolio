d6fa8ff02502a58a4ca0c282074653d699f15e43
jshen12815 <jaebli394@gmail.com>
2020-09-10 09:32:19 -0700

    Other Pages

--- experiments.js
+++ experiments.js
@@ -1,40 +1,13 @@
-
-//Js for side scroll on Homepage
-// get array of pane objects and select last one
-var page = document.getElementById('page');
-var panes = page.getElementsByClassName('pane');
-var last_pane = panes[panes.length-1];
-
-//var expRow = document.getElementById('expRow');
-
-//get X position from left of last panel of first round and last panel
-var endStart = document.getElementById('endStart') //last panel of first round
-var begStar = endStart.getBoundingClientRect().right; //position at the end of first round
-var lastEnd = last_pane.getBoundingClientRect().right; //position at the end of 2nd round
-
+var expRow = document.getElementById('expRow');
 
 window.onscroll = function () {
 
         if(window.matchMedia ("(min-width: 900px)").matches){
         	  // Horizontal Scroll
         	  var y = document.body.getBoundingClientRect().top;
-        	  page.scrollLeft = -y;
-           // expRow.scrollLeft= -y;
-            //document.getElementById('variables').innerHTML = window.scrollY;
+        	  expRow.scrollLeft = -y;
+            document.getElementById('variables').innerHTML = window.scrollY;
         	  
-        	  // Looping Scroll
-              // Find new locations based on window width
-              var begEnd = lastEnd - window.innerWidth;
-              var returnMid = begStar - window.innerWidth;
-
-              //if at 0 position and scroll up
-              if (window.scrollY == 0) {
-                window.scrollTo(0,begStar);
-              }
-              //if at beginning of the last pane
-              else if (window.scrollY == begEnd) {
-                window.scrollTo(0, returnMid);
-              }
           }
               // For testing: 
               // document.getElementById('variables').innerHTML = window.scrollY;
@@ -49,7 +22,7 @@ resize();
 
 // Reset window-based vars
 function resize() {
-        	  var w = page.scrollWidth-window.innerWidth+window.innerHeight;
+        	  var w = expRow.scrollWidth-window.innerWidth+window.innerHeight;
         	  document.body.style.height = w + 'px';
         	}
 
