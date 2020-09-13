c73e1f0a6cca417bbf2cd9a25e2deac45e290073
jshen12815 <jaebli394@gmail.com>
2020-09-13 09:51:24 -0700

    Nav dropscroll

--- index_sidescroll.js
+++ index_sidescroll.js
@@ -20,6 +20,8 @@ var begStar = endStart.getBoundingClientRect().right; //position at the end of f
 var lastEnd = endLast.getBoundingClientRect().right; //position at the end of 2nd round
 
 
+
+
 window.onscroll = function () {
 
         if(window.matchMedia ("(min-width: 480px)").matches){
@@ -28,7 +30,17 @@ window.onscroll = function () {
         	  page.scrollLeft = -y;
            // expRow.scrollLeft= -y;
             //document.getElementById('variables').innerHTML = window.scrollY;
-        	  
+        	  var barP = scrollY / begStar;
+            var barPosition = barP * window.innerWidth;
+
+            var barP2 = (scrollY - begStar) / begStar;
+            var bar2Position = barP2 * window.innerWidth;
+
+
+            /*document.getElementById('variables').innerHTML = begStar;*/
+            document.getElementById("moveBar").style.marginLeft = barPosition + "px";
+            document.getElementById("moveBar2").style.marginLeft = bar2Position + "px";
+
         	  // Looping Scroll
               // Find new locations based on window width
               var begEnd = lastEnd - window.innerWidth;
@@ -44,7 +56,7 @@ window.onscroll = function () {
               }
           }
               // For testing: 
-              // document.getElementById('variables').innerHTML = window.scrollY;
+            // document.getElementById('variables').innerHTML = window.scrollY;  
 
         	}
         	
@@ -58,6 +70,10 @@ resize();
 function resize() {
         	  var w = page.scrollWidth-window.innerWidth+window.innerHeight;
         	  document.body.style.height = w + 'px';
+            var inwidth = window.innerWidth * window.innerWidth
+            var barwidth = inwidth / begStar;
+            document.getElementById("moveBar").style.width = barwidth + "px";
+            document.getElementById("moveBar2").style.width = barwidth + "px";
         	}
 
 function mobileNav() {
@@ -66,6 +82,45 @@ function mobileNav() {
     x.className += " responsive";
   } else {
     x.className = "topnav-right";
+  }
+}
+
+
+dragElement(document.getElementById("moveBar"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos3 = e.clientX;
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
   }
 }
 
